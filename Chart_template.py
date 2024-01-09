2710411c9191afe216feee7f1312067cd24e9f85
Mandana Safari <msafari@sissa.it>
2024-01-09 16:32:24 +0100

    The visualization results in webpages for each code!

--- Chart_template.py
+++ Chart_template.py
@@ -122,8 +122,8 @@ if __name__ == "__main__":
     page_title = "Chart with Data from File"
     # Specify the filenames and associated column names
     dataframes_list = [
-    {'filename': '/home/mandanas/1-CINECA-projects/benchmark/benchmark_10it/bench_7.2dev_gpua_iter/000000/result/result.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes', 'time_unit':'second'},
-    {'filename': '/home/mandanas/1-CINECA-projects/benchmark/benchmark_10it/bench_7.2dev_nogpua_iter/000000/result/result.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes','time_unit':'second'},
+    {'filename': 'result0.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes', 'time_unit':'second'},
+    {'filename': 'result1.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes','time_unit':'second'},
     {'filename': 'results.dat', 'column_name': 'sth_kernel', 'efficiency':'sth_kernel', 'x_axis':'Nodes', 'time_unit':'second'}
     ]
     dataframes = dataframes_maker(dataframes_list)
