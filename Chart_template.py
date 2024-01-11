367f3f6f059a282c6ca47b980166cfb0bce36676
Mandana Safari <msafari@sissa.it>
2024-01-11 15:34:59 +0100

    Flexible input and output available on command line is added successfully

--- Chart_template.py
+++ Chart_template.py
@@ -1,11 +1,12 @@
-
 import jinja2
 import os
 import csv
 import pandas as pd
 import json
-from DF_maker import dataframes_maker
-import shutil
+#from DF_maker import dataframes_maker
+import sys
+import argparse
+import copy
 
 def render_index(tpl_path, context):
     path, filename = os.path.split(tpl_path)
@@ -43,23 +44,26 @@ def render_yambo(tpl_path, context):
     
     return environment.get_template(filename).render(context)
 
-
-def render_template(template_path, output_path, context, file_path=None, column_names=None):
+def render_template(template_path, entry, entry_info, context):
     # Load the template environment
     template_loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(template_path))
     template_env = jinja2.Environment(loader=template_loader, autoescape=True)
-    
-    def load_csv_data(file_path):
-        return pd.read_csv(file_path).to_dict(orient='list')
-    template_env.filters['load_csv_data'] = load_csv_data
-    
-    # Function to modify the CSV data
-    def modify_csv_data(file_path, column_names):
-        # Read CSV into a DataFrame
-        df = pd.read_csv(file_path)
 
-        df['component1'] = df[column_names[file_path]['component1']]
-        df['component2'] = df[column_names[file_path]['component2']]
+
+    def load_csv_data(entry):
+        return entry.to_dict(orient='list')
+
+    template_env.filters['load_csv_data'] = load_csv_data
+
+    context['Lx_axis'] = entry_info['x_axis']
+    context['Lcolumn_name'] = entry_info['column_name']
+    context['Lcomponent'] = entry_info['component']
+    # Function to modify the CSV data
+    def modify_csv_data(entry):
+        df = entry
+        df['x_axis'] = entry['x_axis']
+        df['column_name'] = entry['column_name']
+        df['component'] = entry['component']
         # You can perform other modifications as needed
 
         # Convert the DataFrame to a dictionary
@@ -67,20 +71,26 @@ def render_template(template_path, output_path, context, file_path=None, column_
 
         return modified_data
 
-# Register the function as a Jinja filter
+    # Register the function as a Jinja filter
     template_env.filters['modify_csv_data'] = modify_csv_data
+
     # Load the template
     template = template_env.get_template(os.path.basename(template_path))
-    
-    # Include the file_path in the context
-    context['file_path'] = file_path
-    context['column_names'] = column_names
+
+    # Include the dataframe and column_names in the context
+    context['dataframe'] = entry
+    context['x'] = entry[entry_info['x_axis']]
+    context['column_name'] = entry[entry_info['column_name']]
+    context['component'] = entry[entry_info['component']]
+    context['output_path'] = entry_info['output_path']
+
     # Render the template with the provided context
     output_html = template.render(context)
 
     # Write the rendered HTML to the output file
-    with open(output_path, 'w') as output_file:
+    with open(entry_info['output_path'], 'w') as output_file:
         output_file.write(output_html)
+
 
 def gen_index(flist):
     entries = {}
@@ -117,48 +127,154 @@ def gen_yambo(flist):
         #n+=1
     with open ('yambo.html', 'w') as f:
         f.write(render_yambo('templates/yambo.tmpl', {'entries': entries}))
+
+def count_files(directory):
+    """In order to count the numbers of data_files and get the files in the provided directory"""
+    try:
+        # List all files in the directory
+        files = os.listdir(directory)
+        # Count the number of files
+        file_count = len(files)
+        return file_count, files
+    except OSError:
+        # Handle the case where the directory doesn't exist or is not accessible
+        return -1, []
+
+def code_recogniser(file_path, keywords):
+    # Extract the folder names from the file path
+    folders = file_path.split(os.path.sep)
+
+    # Find the folder with any of the specified keywords
+    found_keywords = [folder for folder in folders if any(keyword.lower() in folder.lower() for keyword in keywords)]
+
+    if found_keywords:
+        print(f"Found keywords {found_keywords} in the folder path.")
+        return found_keywords[0]
+    else:
+        print(f"No specified keywords found in the file path.")
+        return None
+
+def dataframes_maker(dataframes_list, **kwargs):
+    loaded_dataframes = {}
+    for num, df_dict in enumerate(dataframes_list, start=1):
+        df_name = f'df_{num}'
+        dataframe = pd.read_csv(df_dict['filename'])
         
+        # Add new information to the dataframe dictionary
+        dataframe_info = {
+            'dataframe': dataframe,
+            'x_axis': df_dict['x_axis'],
+            'column_name': df_dict['column_name'],
+            'time_unit': df_dict['time_unit'],
+            'component': df_dict['component'],
+            'code': code_recogniser(df_dict['filename'], keywords_to_search),
+            'output_path': kwargs.get('output_path', f'./output_chart{num}.html'),
+            #'column_names': {df_dict['column_name']: ' '},  # Adjust as needed
+        }
+        
+        loaded_dataframes[df_name] = copy.deepcopy(dataframe_info)
+
+        # Add more items if needed
+       # loaded_dataframes[df_name]['new_item'] = 'some_value'
+
+    return loaded_dataframes
+
 if __name__ == "__main__":
     # Define the data for filling in the template
     page_title = "Chart with Data from File"
-    # Specify the filenames and associated column names
-    dataframes_list = [
-    {'filename': 'data/result0.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes', 'time_unit':'second'},
-    {'filename': 'data/result1.dat', 'column_name': 'electrons', 'efficiency':'electrons', 'x_axis':'Nodes','time_unit':'second'},
-    {'filename': 'data/results.dat', 'column_name': 'sth_kernel', 'efficiency':'sth_kernel', 'x_axis':'Nodes', 'time_unit':'second'}
-    ]
-    dataframes = dataframes_maker(dataframes_list)
 
-    data_file_path = ["./df_1.txt", "./df_2.txt", "./df_3.txt"]
+    parser = argparse.ArgumentParser(description='Count the number of files in a directory and process individual file arguments.')
+    parser.add_argument('--directory', '-d', type=str, help='Path to the directory (optional)')
+    parser.add_argument('--files', nargs='+', type=str, help='List of file specifications X_AXIS, COLUMN_NAME, TIME_UNIT, COMPONENT')
+    parser.add_argument('--x_axis', type=str, help='X-axis variable (optional) for all the files in a directory')
+    parser.add_argument('--column_name', type=str, help='Column name for plotting (optional) for all the files in a directory')
+    parser.add_argument('--time_unit', type=str, help='Time unit for plotting (optional) for all the files in a directory')
+    parser.add_argument('--component', nargs='+', type=str, help='Component for plotting (optional) for all the files in a directory')
+
+    args = parser.parse_args()
     
-    # Define the output path for the rendered HTML
-    output_path = ["./output_chart1.html", "./output_chart2.html", "./output_chart3.html"]
-    code = ['Quantum ESPRESSO', 'Yambo']
-    # Define the column names for each file
-    column_names = {
-        "./df_1.txt": {'component1': 'electrons', 'component2': 'other components (df_1)'},
-        "./df_2.txt": {'component1': 'electrons', 'component2': 'other components (df_2)'},
-        "./df_3.txt": {'component1': 'sth_kernel', 'component2': 'other components (df_3)'},
-    }
+    dataframes_list=[]
+
+    keywords_to_search = ['yambo', 'qe', 'othercode']
+    
+    if args.directory:
+        file_count, files = count_files(args.directory)
+        if file_count == -1:
+            print(f"Error: Unable to access or find the directory {args.directory}.")
+        else:
+            print(f"The number of files in the directory {args.directory} is: {file_count}")
+            #process_files(files, args.x_axis, args.column_name, args.time_unit, args.component)
+            for file_path in files:
+                print(f"Processing file: {file_path}")
+                x_axis = args.x_axis or 'Nodes'
+                column_name = args.column_name or 'walltime'
+                time_unit = args.time_unit or 'second'
+                component = args.component or 'empty'
+                
+                # Extract code from the folder structure
+                code = None
+
+                code = code_recogniser(args.directory, keywords_to_search)
+                # Add your logic to use x_axis, column_name, time_unit, and component for each file
+                dataframes_list.append({'filename': args.directory+'/'+file_path, 'x_axis': x_axis, 'column_name': column_name, 'time_unit': time_unit, 'component': component, 'code': code})
+
+    elif args.files:
+        for file_spec in args.files:
+            file_info = {}
+            file_parts = file_spec.split('=')
+            file_info['filename'] = file_parts[0]
+
+            if len(file_parts) > 1:
+                properties = file_parts[1].split(',')
+                file_info['x_axis'] = properties[0] or 'Nodes'
+                file_info['column_name'] = properties[1] or 'walltime'
+                file_info['time_unit'] = properties[2] or 'second'
+                file_info['component'] = properties[3] if properties[3] != 'empty' else None
+
+            # Extract code from the folder structure
+            code = None
+
+            code = code_recogniser(file_info['filename'], keywords_to_search)
+
+            file_info['code'] = code
+                
+            dataframes_list.append(file_info)
+
+    else:
+        print("Please provide either --directory or file paths as arguments.")
+
+    print(dataframes_list)
+
+    loaded_dataframes = dataframes_maker(dataframes_list)
 
     # Create the context to be used in the template
     context = {
         'page_title': page_title,
-        'output_path': output_path,
-       # 'code': code,
     }
     # Render the template and save the output HTML
     filenames = []
-    for file_path, output_file in list(zip(data_file_path, output_path)):
-        render_template("templates/chart_modify.tmpl", output_file, context, file_path=file_path, column_names = column_names)
-        print(file_path)
-        filenames.append(output_file)
+    # Accessing the dataframes and associated information
+    for df_name, df_info in loaded_dataframes.items():
+        dataframe = df_info['dataframe']
+        x_axis = df_info['x_axis']
+        column_name = df_info['column_name']
+        time_unit = df_info['time_unit']
+        component = df_info['component']
+        code = df_info['code']
+        output_path = df_info['output_path']
+
+        print(f"Processing dataframe: {df_name}")
+        print(f"X-axis: {x_axis}, Column Name: {column_name}, Time Unit: {time_unit}, Component: {component}, Code: {code}")
+        print(dataframe)
+        #render_template("templates/chart_modify.tmpl", output_path, context, dataframe=dataframe, column_name = column_name)
+        render_template("templates/chart_modify.tmpl", dataframe, df_info, context)
+        filenames.append(output_path)
 
     gen_index(filenames)
     gen_qe(filenames)
     gen_yambo(filenames)
     
-    print(f"Template has been rendered and saved to {output_path}")
+    print(f"Template has been rendered and saved to {filenames}")
 #it works!
     
     
--- DF_maker.py
+++ DF_maker.py
@@ -3,12 +3,71 @@ import pandas as pd
 import glob
 import copy
 
+def dataframes_maker(dataframes_list, plot='runtime_components', colormap=None, **kwargs):
+    """
+    Load dataframes from a list of dictionaries.
 
-# Define a list of color tuples for each colormap
-colors_list = [("#2EA6A6", "#F28F38"), ("#FF5733", "#33FF57"), ("#FF5733", "#3377FF")]
+    Args:
+        dataframes_list (list): A list of dictionaries where each dictionary contains
+        a dataframe and its associated column name. Also plot the result! it needs to
+        specify the type of Plot, the options are: 'runtime_components' and efficiency. user can
+        specify the desired colormap! or default one in the function.
+
+    Returns:
+        dict: A dictionary where keys are dataframe names and values are the corresponding dataframes and plot.
+
+    Usage:
+        dataframes_maker(dataframes_list, plot='runtime_components/efficiency')
+            Where: dataframes_list = [{'filename':'dat_file1', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},
+                                      {'filename':'dat_file2', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},
+                                      {'filename':'dat_file3', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},...]
+
+    Author:
+        Mandana Safari
+    """
+
+    loaded_dataframes = {}
+    n = 0
+
+    for num, df_dict in enumerate(dataframes_list, start=1):
+        #df_name = f'df_{num}'
+        df_name = f'df_{num}_{df_dict["code"][0]}'  # Include code in df_name
+
+        filename = df_dict['filename']
+
+        filename = df_dict['filename']
+        time_unit = df_dict['time_unit']
+
+        loaded_dataframes[df_name] = copy.deepcopy(pd.read_csv(filename))
+
+        column_name = df_dict['column_name']
+        if column_name not in loaded_dataframes[df_name].columns:
+            raise ValueError(f"Error: Column '{column_name}' does not exist in DataFrame '{df_name}'.")
+
+        component = df_dict.get('component', 'empty')
+
+        x_axis = df_dict['x_axis']
+        if x_axis not in loaded_dataframes[df_name].columns:
+            raise ValueError(f"Error: Column '{x_axis}' does not exist in DataFrame '{df_name}'.")
+
+        loaded_dataframes[df_name]['empty'] = loaded_dataframes[df_name][x_axis] - loaded_dataframes[df_name][x_axis]
+        if component != 'empty':
+            loaded_dataframes[df_name][f'other components of {column_name}'] = loaded_dataframes[df_name][column_name] - loaded_dataframes[df_name][component]
 
 
-def dataframes_maker(dataframes_list, **kwargs):
+        if time_unit == 'second':
+            loaded_dataframes[df_name].iloc[:, 3:] /= 1
+        elif time_unit == 'minute':
+            loaded_dataframes[df_name].iloc[:, 3:] /= 60
+        elif time_unit == 'hour':
+            loaded_dataframes[df_name].iloc[:, 3:] /= 3600
+        else:
+            print(f"Error: Invalid time unit '{time_unit}' specified.")
+
+    return loaded_dataframes
+
+
+def dataframes_maker0(dataframes_list, **kwargs):
     """
     Load dataframes from a list of dictionaries.
     
@@ -22,10 +81,10 @@ def dataframes_maker(dataframes_list, **kwargs):
         dict: A dictionary where keys are dataframe names and values are the corresponding dataframes and plot.
         
     Usage:
-        dataframes_maker(dataframes_list, plot='runtime_components/efficiency') 
-            Where: dataframes_list = [{'filename':'dat_file1', 'column_name':'any_column', 'efficiency':'any_column', 'x_axis':'any_column', 'time_unit':'minute'},
-                                      {'filename':'dat_file2', 'column_name':'any_column', 'efficiency':'any_column', 'x_axis':'any_column', 'time_unit':'minute'},
-                                      {'filename':'dat_file3', 'column_name':'any_column', 'efficiency':'any_column', 'x_axis':'any_column', 'time_unit':'minute'}, ...]
+        dataframes_maker(dataframes_list, plot='runtime_components/efficiency')
+            Where: dataframes_list = [{'filename':'dat_file1', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},
+                                      {'filename':'dat_file2', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},
+                                      {'filename':'dat_file3', 'x_axis':'any_column', 'column_name':'any_column', 'time_unit':'second', 'component':'any_column'},...]
         
     Author:
         Mandana Safari
@@ -34,6 +93,7 @@ def dataframes_maker(dataframes_list, **kwargs):
     loaded_dataframes = {}
     n=0
     for num, df_dict in enumerate(dataframes_list, start=1):
+
         df_name = f'df_{num}'
         dataframe = df_dict
         filename = df_dict['filename']
@@ -43,26 +103,28 @@ def dataframes_maker(dataframes_list, **kwargs):
         column_name = df_dict['column_name']
         if column_name not in loaded_dataframes[df_name].columns:
             raise ValueError(f"Error: Column '{column_name}' does not exist in DataFrame '{df_name}'.")
-            
         
-        efficiency = df_dict['efficiency']
-        if efficiency not in loaded_dataframes[df_name].columns:
-            raise ValueError(f"Error: Column '{efficiency}' does not exist in DataFrame '{df_name}'.")
+        component = df_dict['component']
+   
+        #efficiency = df_dict['efficiency']
+        #if efficiency not in loaded_dataframes[df_name].columns:
+            #raise ValueError(f"Error: Column '{efficiency}' does not exist in DataFrame '{df_name}'.")
             
         x_axis = df_dict['x_axis']
         if x_axis not in loaded_dataframes[df_name].columns:
             raise ValueError(f"Error: Column '{x_axis}' does not exist in DataFrame '{df_name}'.")
-            
-     
-        loaded_dataframes[df_name][f'other components ({df_name})'] = loaded_dataframes[df_name]['walltime'] - loaded_dataframes[df_name][column_name]
-        loaded_dataframes[df_name][f'{column_name} of ({df_name})'] = loaded_dataframes[df_name][column_name]
         
+        loaded_dataframes[df_name]['empty'] = loaded_dataframes[df_name][x_axis] - loaded_dataframes[df_name][x_axis]
+        if df_dict['component'] != 'empty':
+            loaded_dataframes[df_name][f'other components of {column_name}'] = loaded_dataframes[df_name][column_name] - loaded_dataframes[df_name][df_dict['component']]
+
+
                         
-        T1 = loaded_dataframes[df_name][efficiency][loaded_dataframes[df_name][x_axis]== loaded_dataframes[df_name][x_axis].min()].values[0]
-        Tp = loaded_dataframes[df_name][efficiency].values
+       # T1 = loaded_dataframes[df_name][efficiency][loaded_dataframes[df_name][x_axis]== loaded_dataframes[df_name][x_axis].min()].values[0]
+       # Tp = loaded_dataframes[df_name][efficiency].values
         # Calculate speedup and efficiency
-        loaded_dataframes[df_name]['speedup'] = T1 / Tp
-        loaded_dataframes[df_name][f'efficiency of {efficiency}'] = (T1 / Tp) *( loaded_dataframes[df_name][x_axis].min()/loaded_dataframes[df_name][x_axis]) * 100
+       # loaded_dataframes[df_name]['speedup'] = T1 / Tp
+       # loaded_dataframes[df_name][f'efficiency of {efficiency}'] = (T1 / Tp) *( loaded_dataframes[df_name][x_axis].min()/loaded_dataframes[df_name][x_axis]) * 100
 
 
         if time_unit == 'second':
@@ -74,6 +136,7 @@ def dataframes_maker(dataframes_list, **kwargs):
         else:
             print(f"Error: Invalid time unit '{time_unit}' specified.")
         
+
         
         # Save DataFrame to text file (tab-separated, change separator if needed)
         txt_filename = f'{df_name}.txt'
@@ -84,3 +147,41 @@ def dataframes_maker(dataframes_list, **kwargs):
 
 
 
+def render_template(template_path, output_path, context, dataframe=None, column_names=None):
+    # Load the template environment
+    template_loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(template_path))
+    template_env = jinja2.Environment(loader=template_loader, autoescape=True)
+    
+    def load_csv_data(dataframe):
+        return pd.read_csv(dataframe).to_dict(orient='list')
+    template_env.filters['load_csv_data'] = load_csv_data
+    
+    # Function to modify the CSV data
+    def modify_csv_data(dataframe, column_names):
+        # Read CSV into a DataFrame
+        #df = pd.read_csv(file_path)
+
+        df['column_name'] = df[column_names[dataframe]['column_name']]
+        df['column_name'] = df[column_names[dataframe]['component']]
+        # You can perform other modifications as needed
+
+        # Convert the DataFrame to a dictionary
+        modified_data = df.to_dict(orient='list')
+
+        return modified_data
+
+# Register the function as a Jinja filter
+    template_env.filters['modify_csv_data'] = modify_csv_data
+    # Load the template
+    template = template_env.get_template(os.path.basename(template_path))
+    
+    # Include the file_path in the context
+    context['dataframe'] = dataframe
+    context['column_names'] = column_names
+    context['output_path'] = output_path
+    # Render the template with the provided context
+    output_html = template.render(context)
+
+    # Write the rendered HTML to the output file
+    with open(output_path, 'w') as output_file:
+        output_file.write(output_html)
